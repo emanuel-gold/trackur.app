c64c79d6127c61b0f667d79933b96a26c39aa4fd
manny <96850688+emanuel-gold@users.noreply.github.com>
2026-04-24 10:57:46 -0400

    refactor: changing TouchTarget minimum size to 48px

--- src/components/catalyst/button.jsx
+++ src/components/catalyst/button.jsx
@@ -153,7 +153,7 @@ export function TouchTarget({ children }) {
   return (
     <>
       <span
-        className="absolute top-1/2 left-1/2 size-[max(100%,2.75rem)] -translate-x-1/2 -translate-y-1/2 pointer-fine:hidden"
+        className="absolute top-1/2 left-1/2 size-[max(100%,48px)] -translate-x-1/2 -translate-y-1/2 pointer-fine:hidden"
         aria-hidden="true"
       />
       {children}
